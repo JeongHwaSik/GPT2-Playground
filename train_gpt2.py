ac75613da6cc447b85a770ec6992187586b1f7d2
JeongHwaSik <99hwasikk@gmail.com>
2024-10-09 15:43:58 +0900

    apply flash attention

--- train_gpt2.py
+++ train_gpt2.py
@@ -280,6 +280,8 @@ if __name__ == '__main__':
     # ☠️ model compile (think of it like gcc)
     model = torch.compile(model)
 
+    print("FLASHATTENTION")
+
     # LR scheduler
     def get_lr(iter):
         """
