e1137fa51908eb1761013edb2ee1aa6018848cec
JeongHwaSik <99hwasikk@gmail.com>
2024-12-11 21:44:14 +0900

    tokenizer: add regular expression to force not to be merged

--- tokenizer.py
+++ tokenizer.py
@@ -15,6 +15,7 @@ class BPETokenizer:
         self.merges = {}  # merged (pair: idx) for encoding
         self.vocabs_cache = {idx: bytes([idx]) for idx in range(255)}  # vocabs_cache for decoding
 
+        # regular expression forcing not to merge certain pattern!
         self.gpt2pat = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
 
     def train(self, text):
@@ -25,10 +26,19 @@ class BPETokenizer:
         ids = self._get_ids(text)
 
         for k in range(self.num_merges):
-            stats = self._get_stats(ids)  # {pair: count}
-            top_pair = max(stats, key=stats.get)
             idx = k + 256
-            ids = self._merge(ids, top_pair, idx)
+            stats = {}
+            for id in ids:
+                stats.update(self._get_stats(id))  # {pair: count}
+            top_pair = max(stats, key=stats.get)
+
+            new_ids = []
+            for id in ids:
+                if len(id) < 2:
+                    new_ids.append(id)
+                else:
+                    new_ids.append(self._merge(id, top_pair, idx))
+            ids = new_ids
 
             self.merges[top_pair] = idx
             print(f"merging {top_pair} into a new token {idx}")
@@ -42,34 +52,47 @@ class BPETokenizer:
     def encode(self, text):
         ids = self._get_ids(text)
 
-        while True:
-            stats = self._get_stats(ids)
-            if len(stats) == 0:
-                break
-            pair = min(stats, key=lambda pair: self.merges.get(pair, float("inf")))  # min idx in the merges
-            if pair not in self.merges:
-                break
-            idx = self.merges[pair]
-            ids = self._merge(ids, pair, idx)
+        new_ids = []
+        for id in ids:
+            while True:
+                stat = self._get_stats(id)
+                if len(stat) == 0:
+                    new_ids.extend(id)
+                    break
+                pair = min(stat, key=lambda pair: self.merges.get(pair, float("inf")))  # min idx in the merges
+                if pair not in self.merges:
+                    new_ids.extend(id)
+                    break
+                idx = self.merges[pair]
+                new_ids.extend(self._merge(id, pair, idx))
 
-        return ids
+        return new_ids
 
     def decode(self, ids):
         text = b"".join([self.vocabs_cache[idx] for idx in ids])
         text = text.decode("utf-8", errors="replace")
         return text
 
-    @staticmethod
-    def _get_ids(text):
-        tokens = text.encode("utf-8")
-        return list(map(int, tokens))
+    def _get_ids(self, text):
+        pat_text = re.findall(self.gpt2pat, text)
+
+        tokens = []
+        for t in pat_text:
+            tokens.append(list(map(int, t.encode("utf-8"))))
+
+        # tokens = text.encode("utf-8")
+        # return list(map(int, tokens))
+        return tokens
 
     @staticmethod
     def _get_stats(ids):
         counts = {}
-        for pair in zip(ids[:-1], ids[1:]):
-            counts[pair] = counts.get(pair, 0) + 1
-        return counts
+        if len(ids) < 2:
+            return counts
+        else:
+            for pair in zip(ids[:-1], ids[1:]):
+                counts[pair] = counts.get(pair, 0) + 1
+            return counts
 
     @staticmethod
     def _merge(ids, top_pair, idx):
